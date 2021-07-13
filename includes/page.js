485d66afb05e55803479c6443a7acd15d39cce33
Phil Hawksworth <phil@netlify.com>
2021-07-13 12:05:25 +0100

    page semantics

--- includes/page.js
+++ includes/page.js
@@ -8,17 +8,35 @@ module.exports = (data) => {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>${ data.shortURL }</title>
     <style>
+      
+      main,
+      header,
+      footer {
+        text-align: center;
+      }
+      
       img {
         width: 80%;
         margin: 3em auto;
       }
+
+      main {
+        
+      }
+    .dest {
+      color:#666;
+    }
+
     </style
   </head>
   <body>
-    <h1><a href="${ data.shortURL }">${ data.shortURL }</a></h1>
-    <a href="${ data.destinationURL }">${ data.destinationURL }</a><h2></h2>
-    <img src='data:image/svg+xml;utf8,${ data.svg }'>
-
+    <header>
+      <h1><a href="${ data.shortURL }">${ data.shortURL }</a></h1>
+    </header>    
+    <main>
+      <img src='data:image/svg+xml;utf8,${ data.svg }'>
+      <a class="dest" href="${ data.destinationURL }">${ data.destinationURL }</a>
+    </main>
     <footer>
       A URL shortener made with Netlify Redirects by <a href="https://twitter.com/philhawksworth">Phil Hawksworth</a>. Read how to make your own in <a href="https://findthat.at">this blog post</a>
     </footer>
